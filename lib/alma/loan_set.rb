c3254c6df0e875c0ebbe14d4fd086bcb2ddd9965
Chad Nelson <chadbnelson@gmail.com>
2017-01-23 12:10:42 -0500

    Use string in  loan set record count (#3)

--- lib/alma/loan_set.rb
+++ lib/alma/loan_set.rb
@@ -5,7 +5,7 @@ module Alma
 
     def initialize(ws_response)
       @ws_response = ws_response
-      @total_record_count = ws_response.fetch(total_record_count, 0)
+      @total_record_count = ws_response.fetch('total_record_count', 0)
       @list ||= list_results
     end
 
