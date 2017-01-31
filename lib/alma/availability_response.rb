545b95f34c238d59d9234daee8b29760219b0626
Chad Nelson <chad.nelson@temple.edu>
2017-01-31 11:40:42 -0500

    Wrap availability response

--- lib/alma/availability_response.rb
+++ lib/alma/availability_response.rb
@@ -48,37 +48,36 @@ module Alma
 
 
 
-      bibs.map do |bib|
-        record = Hash.new
+      {availability:
+           bibs.map do |bib|
+             record = Hash.new
 
 
-        record['mms_id'] = bib.id
+             record['mms_id'] = bib.id
 
 
+             inventory_fields = bib.record.fetch('datafield', []).select { |df| inventory_types.member?(df['tag']) } || []
 
+             record['holdings'] = inventory_fields.map do |inventory_field|
+               inventory_type = inventory_field['tag']
+               subfield_codes_to_fieldnames = @inventory_type_to_subfield_codes_to_fieldnames[inventory_type]
 
-        inventory_fields = bib.record.fetch('datafield', []).select { |df| inventory_types.member?(df['tag']) } || []
+               # make sure subfields is always an Array (which isn't the case if there's only one subfield element)
+               subfields = [inventory_field.fetch('subfield', [])].flatten(1)
 
-        record['holdings'] = inventory_fields.map do |inventory_field|
-          inventory_type = inventory_field['tag']
-          subfield_codes_to_fieldnames = @inventory_type_to_subfield_codes_to_fieldnames[inventory_type]
-
-          # make sure subfields is always an Array (which isn't the case if there's only one subfield element)
-          subfields = [ inventory_field.fetch('subfield', []) ].flatten(1)
-
-          holding = subfields.reduce(Hash.new) do |acc, subfield|
-            fieldname = subfield_codes_to_fieldnames[subfield['code']]
-            acc[fieldname] = subfield['__content__']
-            acc
-          end
-          holding['inventory_type'] = subfield_codes_to_fieldnames['INVENTORY_TYPE']
-          holding
-        end
-        record
-      end.reduce(Hash.new) do |acc, avail|
-        acc[avail['mms_id']] = avail.select { |k,v| k != 'mms_id' }
-        acc
-      end
+               holding = subfields.reduce(Hash.new) do |acc, subfield|
+                 fieldname = subfield_codes_to_fieldnames[subfield['code']]
+                 acc[fieldname] = subfield['__content__']
+                 acc
+               end
+               holding['inventory_type'] = subfield_codes_to_fieldnames['INVENTORY_TYPE']
+               holding
+             end
+             record
+           end.reduce(Hash.new) do |acc, avail|
+             acc[avail['mms_id']] = avail.select { |k, v| k != 'mms_id' }
+             acc
+           end}
     end
 
 
