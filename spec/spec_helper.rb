8d3e8794424233d9914b603813260584f4b39825
Chad Nelson <chad.nelson@temple.edu>
2018-05-12 14:38:48 -0400

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "alma"
 require 'pry'
 require 'webmock/rspec'
+require 'simplecov'
+SimpleCov.start
 
 SPEC_ROOT = File.dirname __FILE__
 
