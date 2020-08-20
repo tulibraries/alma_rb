72c28febc34b2bf772cc024643d1907cb92383b9
Brian Hoffman <brian.hoffman0003@temple.edu>
2020-08-20 13:42:49 -0400

    change how BibItemSet iterates over Alma api (take 2) (#60)
    * eliminates extra calls to the page after the last page when fetching alma item data
    
    fix error on empty sets
    
    * minor tweak and test for upstream error

--- spec/lib/bib_item_set_spec.rb
+++ spec/lib/bib_item_set_spec.rb
@@ -44,17 +44,42 @@ describe Alma::BibItemSet do
       it 'should make three calls to the api' do
         # Our fixture object has 290 records
         bib_item_set.all.map(&:item_data)
-        expect(WebMock).to have_requested(:get, /.*\.exlibrisgroup\.com\/almaws\/v1\/bibs\/991026207509703811\/holdings\/.*\/items/).times(4)
+        expect(WebMock).to have_requested(:get, /.*\.exlibrisgroup\.com\/almaws\/v1\/bibs\/991026207509703811\/holdings\/.*\/items/).times(3)
       end
 
       it "loops over multiple pages of results" do
         expect(bib_item_set.all.map(&:item_data).size).to eql 290
+      end
+
+      it "doesn't error if the results are empty" do
+        response = instance_double(HTTParty::Response)
+        request = instance_double(HTTParty::Request)
+        allow(response).to receive(:request).and_return request
+        allow(request).to receive(:uri).and_return "http://example.com"
+        allow(response).to receive(:code).and_return 200
+        allow(response).to receive(:parsed_response).and_return({"total_record_count" => 1})
+        empty_bib_set = Alma::BibItemSet.new(response)
+        allow(empty_bib_set).to receive(:empty?).and_return(true)
+        allow(empty_bib_set).to receive(:all).and_call_original
+        expect { empty_bib_set.all.to_a }.not_to raise_error
       end
     end
 
     describe "success?" do
       it "returns true when response code is 200" do
         expect(bib_item_set.success?).to be true
+      end
+    end
+
+    context "BibItemSet is initialized with a failed request" do
+      it "raises a ResponseError" do
+        response = instance_double(HTTParty::Response)
+        request = instance_double(HTTParty::Request)
+        allow(response).to receive(:request).and_return request
+        allow(request).to receive(:uri).and_return "http://example.com"
+        allow(response).to receive(:code).and_return 500
+        allow(response).to receive(:parsed_response).and_return({"total_record_count" => 1})
+        expect { Alma::BibItemSet.new(response) }.to raise_error(Alma::BibItemSet::ResponseError)
       end
     end
   end
